529b5cd53807e2bc7fa4f8c25ebf550271c8d64d
Alexandre Victor <169073432+alexandrevictor-dev@users.noreply.github.com>
2026-06-24 10:47:21 -0300

    Adiciona tabela appointments no schema.sql

--- database/schema.sql
+++ database/schema.sql
@@ -33,9 +33,21 @@ CREATE TABLE IF NOT EXISTS doctors (
     zip_code CHAR(8) NOT NULL,
     medical_specialty TINYINT NOT NULL COMMENT '1: Clínico, 2: Pediatria, 3: Ortopedia, 4: Cardiologia, 5:Psiquiatria, 6: Outros',
     hire_date DATE NOT NULL,
-    termination_date DATE DEFAULT NULL,
-    --Coluna virtual gerada automaticamente pelo MySQL caso haja data de desligamento--
+    termination_date DATE DEFAULT NULL, --Coluna virtual gerada automaticamente pelo MySQL caso haja data de desligamento
     status VARCHAR(10) GENERATED ALWAYS AS (
         CASE WHEN termination_date IS NULL THEN 'Activate' ELSE 'Inactivate' END
     ) STORED
+) ENGINE=InnoDB;
+
+-- Estrutura de Tabela: appointments
+CREATE TABLE IF NOT EXISTS appointments (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    code CHAR(7) NOT NULL UNIQUE,
+    appointment_date DATE NOT NULL,
+    appointment_time TIME NOT NULL,
+    price DECIMAL(6,2) NOT NULL, --O preço só pode ter 6 digitos e 2 para centavos (9999,99)
+    doctor_id INT NOT NULL,
+    patient_id INT NOT NULL,
+    CONSTRAINT fk_appointments_doctor FOREIGN KEY (doctor_id) REFERENCES doctors (id) ON DELETE RESTRICT,
+    CONSTRAINT fk_appointments_patient FOREIGN KEY (patient_id) REFERENCES patients (id) ON DELETE RESTRICT
 ) ENGINE=InnoDB;
